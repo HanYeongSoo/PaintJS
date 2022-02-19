e63173b754916e3de3119069bc996140b49e37c0
HanYeongSoo <86340380+HanYeongSoo@users.noreply.github.com>
2022-02-19 16:57:59 +0900

    2D Context
    #2.1

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const canvas = document.querySelector("#jsCanvas");
+const ctx = canvas.getContext("2D"); // canvasAPI를 위한 첫 선언
+
+ctx.strokeStyle = "#2c2c2c"; // 그림을 처음 시작 할 시 색상
+ctx.lineWidth = 2.5; // 그림을 처음 시작할 때 기본 굵기 설정
 
 let painting = false;
 
@@ -6,24 +10,32 @@ function stopPainting() {
   painting = false;
 }
 
+function startPainting() {
+  painting = true;
+}
+
 function onMouseMove(event) {
   //   console.log(event);
-  const offsetX = event.offsetX;
-  const offsetY = event.offsetY;
+  const x = event.offsetX;
+  const y = event.offsetY;
   //   console.log(offsetX, offsetY);
+
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
 function onMouseDown(event) {
   painting = true;
 }
 
-function onMouseUp(event) {
-  stopPainting();
-}
-
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
